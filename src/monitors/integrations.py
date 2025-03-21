bc3a0c4eee0f7d9fb0d2252d3339379b87289dc5
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2025-03-20 22:28:13 -0400

    Message for fixing yield

--- src/monitors/integrations.py
+++ src/monitors/integrations.py
@@ -1,3 +1,4 @@
+import datetime
 from bots.util import *
 from constants.spectra import SPECTRA_SPINTO_POOLS
 from data_access.contracts.bean import BeanClient
@@ -110,9 +111,61 @@ class IntegrationsMonitor(Monitor):
         pool_client = CurveSpectraClient(spectra_pool, block_number='latest')#TODO
         spinto_client = WrappedDepositClient(spectra_pool.ibt, spectra_pool.underlying, block_number=event_log.blockNumber)
 
+        token_infos = [get_erc20_info(spectra_pool.ibt), get_erc20_info(spectra_pool.pt)]
+        underlying_erc20_info = get_erc20_info(spectra_pool.underlying)
+
         ibt_to_pt_rate = pool_client.get_ibt_to_pt_rate()
         ibt_to_underlying_rate = spinto_client.get_redeem_rate()
         underlying_to_pt_rate = ibt_to_pt_rate / ibt_to_underlying_rate
 
-        
-        pass
+        msg_case = 0
+        if event_log.event == "TokenExchange":
+            sold_id = event_log.args.get("sold_id")
+            tokens_sold = event_log.args.get("tokens_sold")
+            bought_id = event_log.args.get("bought_id")
+            tokens_bought = event_log.args.get("tokens_bought")
+
+            tokens_sold_str = f"{round_token(tokens_sold, token_infos[sold_id].decimals, token_infos[sold_id].addr)} {token_infos[sold_id].symbol}"
+            tokens_bought_str = f"{round_token(tokens_bought, token_infos[bought_id].decimals, token_infos[bought_id].addr)} {token_infos[bought_id].symbol}"
+
+            apy_direction = "📉" if sold_id == 0 else "📈"
+            if sold_id == 1:
+                msg_case += 1
+
+            # TODO: Check if YT was minted/burned to narrow case
+
+            if msg_case == 0:
+                ibt_underlying = tokens_sold * ibt_to_underlying_rate
+                pt_underlying = tokens_bought
+                # Intentionally uses sold token decimals and underlying address. This is because tokens_sold is in terms of
+                # the ibt, and ibt_to_underlying_rate does not have decimals applied.
+                ibt_underlying_str = round_token(ibt_underlying, token_infos[sold_id].decimals, underlying_erc20_info.addr)
+                pt_underlying_str = round_token(pt_underlying, token_infos[bought_id].decimals, token_infos[bought_id].addr)
+                event_str = (
+                    f"Fixed yield: {round_num((pt_underlying / ibt_underlying - 1) * 100, 2)}%: {ibt_underlying_str} -> {pt_underlying_str} {underlying_erc20_info.symbol} "
+                    f"(bought {tokens_bought_str} with {tokens_sold_str})"
+                )
+            elif msg_case == 1:
+                pass
+            elif msg_case == 2:
+                pass
+            elif msg_case == 3:
+                pass
+
+            maturity_str = "Matures" if msg_case < 2 else "Expires"
+
+            hours_to_maturity = (spectra_pool.maturity - datetime.datetime.now(datetime.timezone.utc)).total_seconds() / (60 * 60)
+            apr = ((underlying_to_pt_rate - 1) / hours_to_maturity) * 24 * 365
+            event_str += f"\n> {apy_direction} Implied apy: {round_num(apr * 100, 2)}%. {maturity_str} in {round_num(hours_to_maturity / 24, 0)} days"
+
+        # (0) Fixed yield 10%: 100 -> 110 pinto (bought 110 PT-spinto with 90 spinto)
+        # (1) Exited fixed yield: sold 1000 PT-spinto for 900 spinto (910 pinto)
+        # (2) Exited leveraged yield: sold 1050 YT-spinto for 100 spinto
+        # (3) Leveraged yield 10.5x:  bought 1050 YT-spinto for 100 spinto
+        # > (direction chart) Implied apy: x%. (Matures|Expires) in y days
+
+        # (0) PT: fix yield - swaps sPinto to PT
+        # (1) PT: exit position - swaps PT to sPinto
+        # (2) YT: exit position - swaps sPinto to PT and burns YT
+        # (3) YT: yield leverage - mints YT/PT and swaps PT to sPinto  
+        return event_str
