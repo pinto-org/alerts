426fb454e44c1888ba3521d23d3b55a84ac2b395
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2025-05-23 13:25:30 -0400

    reduce check frequency, twitter post on even seasons only

--- src/monitors/seasons.py
+++ src/monitors/seasons.py
@@ -64,7 +64,9 @@ class SeasonsMonitor(Monitor):
                             sunrise_tx_logs = next(txn.logs for txn in sunrise_swap_logs if txn.txn_hash.hex() == seasonal_sg.beanstalks[0].sunrise_hash)
                             seasonal_sg.beanstalks[0].flood_swap_logs = get_logs_by_names(["Swap"], sunrise_tx_logs)
 
-                self.msg_seasons(self.season_summary_string(seasonal_sg))
+                # Only even seasons go to twitter
+                if not self.short_msgs or seasonal_sg.beanstalks[0].season % 2 == 0:
+                    self.msg_seasons(self.season_summary_string(seasonal_sg))
 
     def _wait_until_expected_sunrise(self):
         """Wait until the top of the hour where a sunrise call is expected"""
