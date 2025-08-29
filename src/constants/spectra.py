13d6b64976150b82be645688888aad82d8749a35
PintoPirate <189064953+PintoPirate@users.noreply.github.com>
2025-08-29 13:32:35 -0400

    Fix new token name format parsing

--- src/constants/spectra.py
+++ src/constants/spectra.py
@@ -28,13 +28,13 @@ SPINTO_1 = SpectraPool(
 # January 15th 2026 maturity
 SPINTO_2 = SpectraPool(
     32965381,
-    datetime.datetime.fromtimestamp(1768518000, datetime.timezone.utc),
-    "0xbcae0acad03b238b97b3158b1fe3eda3c4dc0b83",
-    "0xbcae0acad03b238b97b3158b1fe3eda3c4dc0b83",
-    "0x306e6ec73df3200c62a304cb8d6944e7543fb487",
+    datetime.datetime.fromtimestamp(1768521600, datetime.timezone.utc),
+    "0xbCAe0ACAd03B238b97b3158B1Fe3eDa3c4DC0b83",
+    "0xbCAe0ACAd03B238b97b3158B1Fe3eDa3c4DC0b83",
+    "0x306E6EC73df3200C62a304cb8D6944e7543Fb487",
     "0x52B1fce1784AC9dA1F31E04077F46388CC4AF7b3",
-    "0xb170000aeefa790fa61d6e837d1035906839a3c8",
-    "0x00b174d66ada7d63789087f50a9b9e0e48446dc1",
+    "0xb170000aeeFa790fa61D6e837d1035906839a3c8",
+    "0x00b174d66adA7d63789087F50A9b9e0e48446dc1",
     False
 )
 
